f460969e8659278e29b284f8ed9e3783ae2498a4
GopijaVenepalli <gopijavenepalli@gmail.com>
2022-04-13 23:22:29 -0500

    updated files

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var costumesRouter = require('./routes/costumes');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var Costume = require("./models/costumes");
+var resourceRouter = require("./routes/resource");
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +38,7 @@ app.use('/users', usersRouter);
 app.use('/costumes', costumesRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,3 +57,37 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+// We can seed the collection if needed on server start 
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new 
+Costume({costume_type:"ghost1",  costume_name:'large', 
+units:25.4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+  let instance2 = new 
+Costume({costume_type:"ghost2",  costume_name:'medium', 
+units:67}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Second object saved") 
+  }); 
+
+  let instance3 = new 
+Costume({costume_type:"ghost3",  costume_name:'small', 
+units:98}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Third object saved") 
+  }); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();}
--- routes/costumes.js
+++ routes/costumes.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('costumes', { title: 'Search Results Costumes' });
-});
-
-module.exports = router;
+var express = require('express'); 
+const costume_controlers= require('../controllers/costumes'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', costume_controlers.costume_view_all_Page ); 
+module.exports = router; 
