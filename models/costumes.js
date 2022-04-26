ef03fcfdba19a9894ea53efeffa0653ccba242b6
GopijaVenepalli <gopijavenepalli@gmail.com>
2022-04-25 22:41:11 -0500

    updated files

--- models/costumes.js
+++ models/costumes.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose") 
 const costumeSchema = mongoose.Schema({ 
     costume_type: String, 
-    costume_name: String, 
+    costume_name: {type:String,
+        minLength:2,
+        maxLength:30}, 
     units: {type:Number,
     min:2,
 max:200} 
